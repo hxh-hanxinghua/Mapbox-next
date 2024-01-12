0fb929821618cfa2b903587b8f62d297cd80f73a
hxh-hanxinghua <1758287410@qq.com>
2024-01-12 16:37:01 +0800

    feat: 项目结构调整，增加代码检查

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  singleQuote: false,
+  printWidth: 100,
+  semi: true,
+  trailingComma: "all",
+  arrowParens: "always",
+  bracketSpacing: true,
+  useTabs: false,
+  tabWidth: 2,
+  proseWrap: "always",
+  htmlWhitespaceSensitivity: "css",
+};
